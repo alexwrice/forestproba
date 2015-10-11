838e7ef8e8a6e7a622fa768d5e86136fb4d0a46a
Marc Assens <marc.a.r95@gmail.com>
2015-10-11 23:46:16 +0200

    menu

--- app/models/document.rb
+++ app/models/document.rb
@@ -4,9 +4,8 @@ class Document < ActiveRecord::Base
 	#set up "uploaded_file" field as attached_file (using Paperclip) 
 	has_attached_file :uploaded_file, :url => "/documents/get/:id", :path => ":Rails_root/documents/:id/:basename.:extension"
 	
-	validates_attachment :uploaded_file, presence: true,
-	content_type: { content_type: "image/jpeg" },
-	size: { in: 0..10.megabytes }
+	validates_attachment :uploaded_file, presence: true, size: { in: 0..10.megabytes }
+	do_not_validate_attachment_file_type :uploaded_file
 
 	def file_name 
 		uploaded_file_file_name 
