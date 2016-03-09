1acceadfe4812c274db737e78d7132abc0e6edfa
Marc Assens <marc.a.r95@gmail.com>
2016-03-09 17:06:46 +0100

    PROTO 3
    Diseny basat en el Insivision amb Header
    1ra Versió

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -20,25 +20,7 @@ class HomeController < ApplicationController
 		@current_folder = current_user.folders.find(params[:folder_id])  
 		@clasificacions = current_user.folders.roots  
 
-		@f = params[:f]
-		if @f 
-				#Saltar entre arbres
-				# .parent == nil vol dir que es una clasificació!!
-				# @f.pop if @current_folder.parent == nil && current_user.folders.find(@f.last).parent == nil
-				if current_user.folders.find(@f.last).parent == nil
-					@f.pop
-				end
-
-				#Torna enrere
-				if @f.include?("#{@current_folder.id}")	
-					@f.pop(@f.length-@f.index("#{@current_folder.id}")) 
-				end
-
-				@f.push(@current_folder.id)
-		else
-				@f=[@current_folder.id]
-		end
-
+		@path = update_path(params, @current_folder)
 
 
 		if @current_folder
@@ -47,7 +29,7 @@ class HomeController < ApplicationController
 			#Obtenim documents i seleccionem els que toquen
 			#Hem de passar les ids a numeros, son numbers
 
-			@documents = @current_folder.documents.order("updated_at desc").select{ |d| @f.map(&:to_i).included_in?(d.folder_ids)}.select{ |d| !@current_folder.children.ids.intersects_with?(d.folder_ids)}
+			@documents = @current_folder.documents.order("updated_at desc").select{ |d| @path.map(&:to_i).included_in?(d.folder_ids)}.select{ |d| !@current_folder.children.ids.intersects_with?(d.folder_ids)}
 
 			#FILTREM CARPETES
 			#Estem creuant arbres --> només apareixen carpetes on hi ha resultats
@@ -56,14 +38,14 @@ class HomeController < ApplicationController
 			#REFACTOR
 			#CHAPUZA! per trobar les carpetes creuades es necesiten tots els doucments, no només els que apareixen aquí al mig, i s'ha de crear @documents_all
 			documents_ids = []
-			@documents_all = @current_folder.documents.order("updated_at desc").select{ |d|  @f.map(&:to_i).included_in?(d.folder_ids)}
+			@documents_all = @current_folder.documents.order("updated_at desc").select{ |d|  @path.map(&:to_i).included_in?(d.folder_ids)}
 			@documents_all.each{|d| documents_ids << d.id }
 
 
 			#Només filtrem carpetes si hitop_menu_clasificacions ha més d'1 arbre
 			#roots => noms de les clasificacions
 			@roots = []
-			@f.each{|id| @roots << current_user.folders.find(id).root}
+			@path.each{|id| @roots << current_user.folders.find(id).root}
 			@roots = @roots.uniq
 
 			if @roots.length == 1
@@ -80,5 +62,34 @@ class HomeController < ApplicationController
 			flash[:error] = "Don't be cheeky! Mind your own folders!"
 			redirect_to root_url 
 		end
+	end
+
+
+
+
+	#------ AUXILIAR METHODS  -------
+
+
+	def update_path(passed_params, current_folder)
+		path= passed_params[:path]
+		if path 
+				#Saltar entre arbres
+				# .parent == nil vol dir que es una clasificació!!
+				# path.pop if @current_folder.parent == nil && current_user.folders.find(path.last).parent == nil
+				if current_user.folders.find(path.last).parent == nil
+					path.pop
+				end
+
+				#Torna enrere
+				if path.include?("#{current_folder.id}")	
+					path.pop(path.length-path.index("#{current_folder.id}")) 
+				end
+
+				path.push(current_folder.id)
+		else
+				path=[current_folder.id]
+		end
+
+		return path
 	end
 end
---
+++ devforest.sh
@@ -0,0 +1,3 @@
+cd Dropbox/Forest_Marc\&Enric/3_Prototip/forestproba/
+mysql.server start
+rails server
